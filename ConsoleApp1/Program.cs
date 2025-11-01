8eaad56e891dfc73de2788461b8c657a86247bed
George Zacharias <zachariasg7@hotmail.gr>
2025-11-01 14:00:28 +0200

    first run

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,15 @@ public class Program
     static string studentSubject;
     static double studentGrade;
     static double studentAverage;
+    //make the method that will print the student
+    public void PrintStudentInfo()
+    {
+        Console.WriteLine("Student Name: " + studentName);
+        Console.WriteLine("Student ID: " + studentID);
+        Console.WriteLine("Student Subject: " + studentSubject);
+        Console.WriteLine("Student Grade: " + studentGrade);
+        Console.WriteLine("Student Average: " + studentAverage);
+    }
     public static void Main()
     {
 
