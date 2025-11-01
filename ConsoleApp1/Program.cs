dddce4ff0f1c00541d035e3c1b3aef9780ac1728
George Zacharias <zachariasg7@hotmail.gr>
2025-11-01 14:53:19 +0200

    menu ready

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,13 +6,12 @@ public class Program
     public static void Main()
     {
         //lets make the menu
-        List<string> tasks = new List<string>();
         bool exit = false;
         while (!exit)
         {
             Console.WriteLine("Menu:");
             Console.WriteLine("1. Add Student");
-            Console.WriteLine("2. Add Grade");
+            Console.WriteLine("2. Add Subject");
             Console.WriteLine("3. View Students");
             Console.WriteLine("4. Exit");
             Console.Write("Choose an option: ");
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e661134789d51fd7eb72c14c09936f031b1e082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20d7e9f6289b38fa8df3c4e375403e3d1e9f500c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
