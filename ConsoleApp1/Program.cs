8274130e25769cb4464b41033f800acc541ed39a
George Zacharias <zachariasg7@hotmail.gr>
2025-11-01 13:57:48 +0200

    all the vars we are going to need

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ public class Program
     // defining variables that we will need 
     static string studentName;
     static int studentID;
+    static string studentSubject;
     static double studentGrade;
     static double studentAverage;
     public static void Main()
