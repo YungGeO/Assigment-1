da12cfc890070cf59dccefaa92413f7e2b58d254
George Zacharias <zachariasg7@hotmail.gr>
2025-11-03 12:27:59 +0200

    merged edit grades

--- ConsoleApp1/obj/Debug/netcoreapp8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/netcoreapp8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aecabda419fe699578edf478416ead79f6fecb17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5f1ccdabd50dbb15abcc6a56c3254cf27bb7970")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
